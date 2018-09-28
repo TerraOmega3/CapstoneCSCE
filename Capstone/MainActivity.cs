55cbd1d10dc3c0509d8f282a6c54220100295bd0
Joshua Langley <joshua.s.langley@gmail.com>
2018-09-28 02:32:13 -0500

    Updated Comments to reflect function purpose in Main, updated names/pictures.

--- Capstone/MainActivity.cs
+++ Capstone/MainActivity.cs
@@ -1,57 +1,66 @@
 ﻿using System;
-using Android;
 using Android.App;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V4.View;
 using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 
-namespace App1
+namespace Capstone
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "EyeFi", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            //Creates main page layout by referencing activity_main.axml
             SetContentView(Resource.Layout.activity_main);
+
+            //Displays a three dot vertical button widget which displays a list of actions (in this case none at the moment)
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
+            //Displays the mail button
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
-
+           
+            //Creates the three horizontal bar on top left which brings up Navigation View on toggle
             DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //Opens and closes navigation tab on click
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
             drawer.AddDrawerListener(toggle);
             toggle.SyncState();
 
+            //Allows for the buttons in the navigation tab to work on click
             NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
             navigationView.SetNavigationItemSelectedListener(this);
         }
 
+        //What happens when someone presses the back button?
         public override void OnBackPressed()
         {
             DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //If the navigation side tab is open, close it
             if(drawer.IsDrawerOpen(GravityCompat.Start))
             {
                 drawer.CloseDrawer(GravityCompat.Start);
             }
-            else
+            else //Do nothing
             {
                 base.OnBackPressed();
             }
         }
-
+        
+        //Create a three dot upper tab to go to settings on the upper right of the main page
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
-
+        
+        //Unsure of direct purpose
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
@@ -63,30 +72,30 @@ namespace App1
             return base.OnOptionsItemSelected(item);
         }
 
+        //This is the mail button's function call
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
             View view = (View) sender;
+            //Causes a small bar to appear on the bottom that calls either a function or in this case a "replace" statement
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 
+        //This is the side bar's function calls
         public bool OnNavigationItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
 
-            if (id == Resource.Id.nav_camera)
+            //Creates logo and id name by referencing the file activity_main_drawer.xml which lists each one
+            if (id == Resource.Id.nav_map)
             {
-                // Handle the camera action
+                // Handles the map functions
             }
-            else if (id == Resource.Id.nav_gallery)
+            else if (id == Resource.Id.nav_local)
             {
-
+         
             }
-            else if (id == Resource.Id.nav_slideshow)
-            {
-
-            }
-            else if (id == Resource.Id.nav_manage)
+            else if (id == Resource.Id.nav_settings)
             {
 
             }
