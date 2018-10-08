02384e7a1c7045c9fe26ae3a2a498626cd0ea9f1
Joshua Langley <joshua.s.langley@gmail.com>
2018-10-07 19:32:58 -0500

    Added WifiManager items: RSSI and AP addres. Added button to refresh information seen on screeen.

--- Capstone/MainActivity.cs
+++ Capstone/MainActivity.cs
@@ -1,23 +1,81 @@
 ﻿using System;
+using System.Threading;
 using Android.App;
+using Android.Content;
+using Android.Net;
+using Android.Net.Wifi;
 using Android.OS;
 using Android.Support.Design.Widget;
 using Android.Support.V4.View;
 using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Views;
-
+using Android.Widget;
 namespace Capstone
 {
     [Activity(Label = "EyeFi", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
+        //Change this to your own network ID name or in the schools case "tamulink-wpa"
+        const string networkSSID = "\"" + "Rise.Synergy.Wifi.com" + "\"";
+        
+        TextView wifiText;
+        Button wifiButton;
+        WifiManager wifiManager;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            
             //Creates main page layout by referencing activity_main.axml
             SetContentView(Resource.Layout.activity_main);
 
+            //Everything below simply connects to wifi given the prewritten networkSSID. To test on your own wifi just change it to your wifiName
+            var conf = new WifiConfiguration();
+            conf.Ssid = networkSSID;
+            conf.AllowedKeyManagement.Set((int)KeyManagementType.None);
+            var connectivityManager = (ConnectivityManager)GetSystemService(ConnectivityService);
+            //The NetworkInfo line is technically obsolete so I disabled the portion of code which prevented its use then restored it. Just ignore for now.
+#pragma warning disable CS0618 // Type or member is obsolete
+            NetworkInfo.State mobileState = connectivityManager.GetNetworkInfo(ConnectivityType.Wifi).GetState();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            //Sees if it is connected to a network
+            if (mobileState != NetworkInfo.State.Connected)
+            {
+                var mawifi = (WifiManager)GetSystemService(WifiService);
+                mawifi.SetWifiEnabled(true);
+            }
+            Thread.Sleep(5000);
+            wifiManager = (WifiManager)GetSystemService(WifiService);
+            wifiManager.AddNetwork(conf);
+            var list = wifiManager.ConfiguredNetworks;
+            //Finds the network in your network list and connects to it.
+            foreach (var i in list)
+            {
+                if (i.Ssid != null && i.Ssid.Equals(networkSSID))
+                {
+                    wifiManager.Disconnect();
+                    wifiManager.EnableNetwork(i.NetworkId, true);
+                    wifiManager.Reconnect();
+                    break;
+                }
+            }
+
+            //Number of levels for signal strength
+            int numlevels = 5;
+            //Gather WifiInfo data
+            WifiInfo wifiInfo = wifiManager.ConnectionInfo;
+            int level = WifiManager.CalculateSignalLevel(wifiInfo.Rssi, numlevels);
+            //Connect to the main_text on Content_main layer
+            wifiText = (TextView)FindViewById(Resource.Id.main_text);
+            //Chain text to include wifi info items (Note: rssi, BSSID, and linkspeed can all be called after getting wifiInfo)
+            // BSSID: AP address, LinkSpeed is the internet speed in Mbps, Rssi is in decibals so from -80 to -10 where -10 is a better connection
+            wifiText.Text = networkSSID + "\n RSSI: " + wifiInfo.Rssi.ToString() + "\n WifiSignal (1-5): "+ level + "\n AP SSID: "+ wifiInfo.BSSID +
+            "\n LinkSpeed: " + wifiInfo.LinkSpeed;
+            //Create and connect to button from main_content
+            wifiButton = FindViewById<Button>(Resource.Id.button1);
+            //Calls the WFOnClick() function on press
+            wifiButton.Click += WFOnClick;
             //Displays a three dot vertical button widget which displays a list of actions (in this case none at the moment)
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
@@ -25,7 +83,7 @@ namespace Capstone
             //Displays the mail button
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
-           
+
             //Creates the three horizontal bar on top left which brings up Navigation View on toggle
             DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
             //Opens and closes navigation tab on click
@@ -43,7 +101,7 @@ namespace Capstone
         {
             DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
             //If the navigation side tab is open, close it
-            if(drawer.IsDrawerOpen(GravityCompat.Start))
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
             {
                 drawer.CloseDrawer(GravityCompat.Start);
             }
@@ -52,14 +110,14 @@ namespace Capstone
                 base.OnBackPressed();
             }
         }
-        
+
         //Create a three dot upper tab to go to settings on the upper right of the main page
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
-        
+
         //Unsure of direct purpose
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
@@ -75,12 +133,21 @@ namespace Capstone
         //This is the mail button's function call
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
+            View view = (View)sender;
             //Causes a small bar to appear on the bottom that calls either a function or in this case a "replace" statement
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
+        private void WFOnClick(object sender, EventArgs eventArgs)
+        {
+            int numlevels = 5;
+            WifiInfo wifiInfo = wifiManager.ConnectionInfo;
+            int level = WifiManager.CalculateSignalLevel(wifiInfo.Rssi, numlevels);
+            wifiText.Text = networkSSID + "\n RSSI: " + wifiInfo.Rssi.ToString() + "\n WifiSignal (1-5): " + level + "\n AP SSID: " + wifiInfo.BSSID + 
+            "\n LinkSpeed: " + wifiInfo.LinkSpeed;
+            //Causes a small bar to appear on the bottom that calls either a function or in this case a "replace" statement
 
+        }
         //This is the side bar's function calls
         public bool OnNavigationItemSelected(IMenuItem item)
         {
@@ -90,12 +157,24 @@ namespace Capstone
             if (id == Resource.Id.nav_map)
             {
                 // Handles the map functions
+                Android.Widget.RelativeLayout mainLayout = (Android.Widget.RelativeLayout)FindViewById(Resource.Id.all_container);
+                Android.Widget.RelativeLayout parentLayout = (Android.Widget.RelativeLayout)FindViewById(Resource.Layout.app_bar_main);
+                LayoutInflater inflater = (LayoutInflater)GetSystemService(Context.LayoutInflaterService);
+                View layout = inflater.Inflate(Resource.Layout.content_main, parentLayout);
+                mainLayout.RemoveAllViews();
+                mainLayout.AddView(layout);
+                
             }
-            else if (id == Resource.Id.nav_local)
+            else if (id == Resource.Id.nav_loc)
             {
-         
+                Android.Widget.RelativeLayout mainLayout = (Android.Widget.RelativeLayout)FindViewById(Resource.Id.all_container);
+                LayoutInflater inflater = (LayoutInflater)GetSystemService(Context.LayoutInflaterService);
+                View layout = inflater.Inflate(Resource.Layout.content_local, null);
+                mainLayout.RemoveAllViews();
+                mainLayout.AddView(layout);
+               
             }
-            else if (id == Resource.Id.nav_settings)
+            else if (id == Resource.Id.nav_set)
             {
 
             }
@@ -114,4 +193,3 @@ namespace Capstone
         }
     }
 }
-
--- Capstone/Resources/Resource.Designer.cs
+++ Capstone/Resources/Resource.Designer.cs
@@ -2242,26 +2242,26 @@ namespace Capstone
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f020072
+			public const int avd_hide_password_1 = 2130837618;
+			
 			// aapt resource value: 0x7f020073
-			public const int avd_hide_password_1 = 2130837619;
+			public const int avd_hide_password_2 = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int avd_hide_password_2 = 2130837620;
-			
-			// aapt resource value: 0x7f020075
-			public const int avd_hide_password_3 = 2130837621;
+			public const int avd_hide_password_3 = 2130837620;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
+			// aapt resource value: 0x7f020075
+			public const int avd_show_password_1 = 2130837621;
+			
 			// aapt resource value: 0x7f020076
-			public const int avd_show_password_1 = 2130837622;
+			public const int avd_show_password_2 = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int avd_show_password_2 = 2130837623;
-			
-			// aapt resource value: 0x7f020078
-			public const int avd_show_password_3 = 2130837624;
+			public const int avd_show_password_3 = 2130837623;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2280,9 +2280,6 @@ namespace Capstone
 			
 			// aapt resource value: 0x7f02005a
 			public const int design_snackbar_background = 2130837594;
-			
-			// aapt resource value: 0x7f020072
-			public const int ic_menu_camera = 2130837618;
 			
 			// aapt resource value: 0x7f02005b
 			public const int ic_menu_gallery = 2130837595;
@@ -2384,8 +2381,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f0800a2
-			public const int action0 = 2131230882;
+			// aapt resource value: 0x7f0800a5
+			public const int action0 = 2131230885;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2408,17 +2405,17 @@ namespace Capstone
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f08009f
-			public const int action_container = 2131230879;
+			// aapt resource value: 0x7f0800a2
+			public const int action_container = 2131230882;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int action_divider = 2131230886;
+			// aapt resource value: 0x7f0800a9
+			public const int action_divider = 2131230889;
 			
-			// aapt resource value: 0x7f0800a0
-			public const int action_image = 2131230880;
+			// aapt resource value: 0x7f0800a3
+			public const int action_image = 2131230883;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2435,14 +2432,14 @@ namespace Capstone
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f0800ba
-			public const int action_settings = 2131230906;
+			// aapt resource value: 0x7f0800bd
+			public const int action_settings = 2131230909;
 			
-			// aapt resource value: 0x7f0800a1
-			public const int action_text = 2131230881;
+			// aapt resource value: 0x7f0800a4
+			public const int action_text = 2131230884;
 			
-			// aapt resource value: 0x7f0800af
-			public const int actions = 2131230895;
+			// aapt resource value: 0x7f0800b2
+			public const int actions = 2131230898;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2455,6 +2452,9 @@ namespace Capstone
 			
 			// aapt resource value: 0x7f080052
 			public const int all = 2131230802;
+			
+			// aapt resource value: 0x7f08008e
+			public const int all_container = 2131230862;
 			
 			// aapt resource value: 0x7f080038
 			public const int always = 2131230776;
@@ -2474,11 +2474,14 @@ namespace Capstone
 			// aapt resource value: 0x7f08003d
 			public const int bottom = 2131230781;
 			
+			// aapt resource value: 0x7f080091
+			public const int button1 = 2131230865;
+			
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800a3
-			public const int cancel_action = 2131230883;
+			// aapt resource value: 0x7f0800a6
+			public const int cancel_action = 2131230886;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2492,8 +2495,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int chronometer = 2131230891;
+			// aapt resource value: 0x7f0800ae
+			public const int chronometer = 2131230894;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2504,14 +2507,14 @@ namespace Capstone
 			// aapt resource value: 0x7f080039
 			public const int collapseActionView = 2131230777;
 			
-			// aapt resource value: 0x7f080091
-			public const int container = 2131230865;
+			// aapt resource value: 0x7f080094
+			public const int container = 2131230868;
 			
 			// aapt resource value: 0x7f080066
 			public const int contentPanel = 2131230822;
 			
-			// aapt resource value: 0x7f080092
-			public const int coordinator = 2131230866;
+			// aapt resource value: 0x7f080095
+			public const int coordinator = 2131230869;
 			
 			// aapt resource value: 0x7f08006d
 			public const int custom = 2131230829;
@@ -2525,20 +2528,20 @@ namespace Capstone
 			// aapt resource value: 0x7f080060
 			public const int default_activity_button = 2131230816;
 			
-			// aapt resource value: 0x7f080094
-			public const int design_bottom_sheet = 2131230868;
+			// aapt resource value: 0x7f080097
+			public const int design_bottom_sheet = 2131230871;
+			
+			// aapt resource value: 0x7f08009e
+			public const int design_menu_item_action_area = 2131230878;
+			
+			// aapt resource value: 0x7f08009d
+			public const int design_menu_item_action_area_stub = 2131230877;
+			
+			// aapt resource value: 0x7f08009c
+			public const int design_menu_item_text = 2131230876;
 			
 			// aapt resource value: 0x7f08009b
-			public const int design_menu_item_action_area = 2131230875;
-			
-			// aapt resource value: 0x7f08009a
-			public const int design_menu_item_action_area_stub = 2131230874;
-			
-			// aapt resource value: 0x7f080099
-			public const int design_menu_item_text = 2131230873;
-			
-			// aapt resource value: 0x7f080098
-			public const int design_navigation_view = 2131230872;
+			public const int design_navigation_view = 2131230875;
 			
 			// aapt resource value: 0x7f080020
 			public const int disableHome = 2131230752;
@@ -2552,8 +2555,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int end_padder = 2131230897;
+			// aapt resource value: 0x7f0800b4
+			public const int end_padder = 2131230900;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2570,8 +2573,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080072
 			public const int expanded_menu = 2131230834;
 			
-			// aapt resource value: 0x7f08008e
-			public const int fab = 2131230862;
+			// aapt resource value: 0x7f08008f
+			public const int fab = 2131230863;
 			
 			// aapt resource value: 0x7f080050
 			public const int fill = 2131230800;
@@ -2600,8 +2603,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int icon_group = 2131230896;
+			// aapt resource value: 0x7f0800b3
+			public const int icon_group = 2131230899;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2609,11 +2612,11 @@ namespace Capstone
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f08009d
-			public const int imageView = 2131230877;
+			// aapt resource value: 0x7f0800a0
+			public const int imageView = 2131230880;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int info = 2131230892;
+			// aapt resource value: 0x7f0800af
+			public const int info = 2131230895;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2621,8 +2624,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
-			// aapt resource value: 0x7f080090
-			public const int largeLabel = 2131230864;
+			// aapt resource value: 0x7f080093
+			public const int largeLabel = 2131230867;
 			
 			// aapt resource value: 0x7f080049
 			public const int left = 2131230793;
@@ -2639,14 +2642,17 @@ namespace Capstone
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800b4
-			public const int masked = 2131230900;
+			// aapt resource value: 0x7f080090
+			public const int main_text = 2131230864;
 			
-			// aapt resource value: 0x7f0800a5
-			public const int media_actions = 2131230885;
+			// aapt resource value: 0x7f0800b7
+			public const int masked = 2131230903;
 			
-			// aapt resource value: 0x7f0800b2
-			public const int message = 2131230898;
+			// aapt resource value: 0x7f0800a8
+			public const int media_actions = 2131230888;
+			
+			// aapt resource value: 0x7f0800b5
+			public const int message = 2131230901;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2657,26 +2663,26 @@ namespace Capstone
 			// aapt resource value: 0x7f080028
 			public const int multiply = 2131230760;
 			
-			// aapt resource value: 0x7f0800b6
-			public const int nav_local = 2131230902;
-			
-			// aapt resource value: 0x7f0800b5
-			public const int nav_map = 2131230901;
-			
 			// aapt resource value: 0x7f0800b9
-			public const int nav_send = 2131230905;
-			
-			// aapt resource value: 0x7f0800b7
-			public const int nav_settings = 2131230903;
+			public const int nav_loc = 2131230905;
 			
 			// aapt resource value: 0x7f0800b8
-			public const int nav_share = 2131230904;
+			public const int nav_map = 2131230904;
+			
+			// aapt resource value: 0x7f0800bc
+			public const int nav_send = 2131230908;
+			
+			// aapt resource value: 0x7f0800ba
+			public const int nav_set = 2131230906;
+			
+			// aapt resource value: 0x7f0800bb
+			public const int nav_share = 2131230907;
 			
 			// aapt resource value: 0x7f08008c
 			public const int nav_view = 2131230860;
 			
-			// aapt resource value: 0x7f080097
-			public const int navigation_header_container = 2131230871;
+			// aapt resource value: 0x7f08009a
+			public const int navigation_header_container = 2131230874;
 			
 			// aapt resource value: 0x7f08003b
 			public const int never = 2131230779;
@@ -2687,14 +2693,14 @@ namespace Capstone
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int notification_background = 2131230894;
+			// aapt resource value: 0x7f0800b1
+			public const int notification_background = 2131230897;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int notification_main_column = 2131230888;
+			// aapt resource value: 0x7f0800ab
+			public const int notification_main_column = 2131230891;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int notification_main_column_container = 2131230887;
+			// aapt resource value: 0x7f0800aa
+			public const int notification_main_column_container = 2131230890;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2720,11 +2726,11 @@ namespace Capstone
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int right_icon = 2131230893;
+			// aapt resource value: 0x7f0800b0
+			public const int right_icon = 2131230896;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int right_side = 2131230889;
+			// aapt resource value: 0x7f0800ac
+			public const int right_side = 2131230892;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2798,14 +2804,14 @@ namespace Capstone
 			// aapt resource value: 0x7f080025
 			public const int showTitle = 2131230757;
 			
-			// aapt resource value: 0x7f08008f
-			public const int smallLabel = 2131230863;
+			// aapt resource value: 0x7f080092
+			public const int smallLabel = 2131230866;
 			
-			// aapt resource value: 0x7f080096
-			public const int snackbar_action = 2131230870;
+			// aapt resource value: 0x7f080099
+			public const int snackbar_action = 2131230873;
 			
-			// aapt resource value: 0x7f080095
-			public const int snackbar_text = 2131230869;
+			// aapt resource value: 0x7f080098
+			public const int snackbar_text = 2131230872;
 			
 			// aapt resource value: 0x7f080043
 			public const int snap = 2131230787;
@@ -2828,8 +2834,8 @@ namespace Capstone
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800a4
-			public const int status_bar_latest_event_content = 2131230884;
+			// aapt resource value: 0x7f0800a7
+			public const int status_bar_latest_event_content = 2131230887;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2855,11 +2861,11 @@ namespace Capstone
 			// aapt resource value: 0x7f080069
 			public const int textSpacerNoTitle = 2131230825;
 			
-			// aapt resource value: 0x7f08009e
-			public const int textView = 2131230878;
+			// aapt resource value: 0x7f0800a1
+			public const int textView = 2131230881;
 			
-			// aapt resource value: 0x7f08009c
-			public const int text_input_password_toggle = 2131230876;
+			// aapt resource value: 0x7f08009f
+			public const int text_input_password_toggle = 2131230879;
 			
 			// aapt resource value: 0x7f080014
 			public const int textinput_counter = 2131230740;
@@ -2867,8 +2873,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int time = 2131230890;
+			// aapt resource value: 0x7f0800ad
+			public const int time = 2131230893;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2888,8 +2894,8 @@ namespace Capstone
 			// aapt resource value: 0x7f08006e
 			public const int topPanel = 2131230830;
 			
-			// aapt resource value: 0x7f080093
-			public const int touch_outside = 2131230867;
+			// aapt resource value: 0x7f080096
+			public const int touch_outside = 2131230870;
 			
 			// aapt resource value: 0x7f08000f
 			public const int transition_current_scene = 2131230735;
@@ -2918,8 +2924,8 @@ namespace Capstone
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b3
-			public const int visible = 2131230899;
+			// aapt resource value: 0x7f0800b6
+			public const int visible = 2131230902;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
@@ -3062,13 +3068,13 @@ namespace Capstone
 			public const int abc_select_dialog_material = 2130968601;
 			
 			// aapt resource value: 0x7f04001a
-			public const int activity_local = 2130968602;
+			public const int activity_main = 2130968602;
 			
 			// aapt resource value: 0x7f04001b
-			public const int activity_main = 2130968603;
+			public const int app_bar_main = 2130968603;
 			
 			// aapt resource value: 0x7f04001c
-			public const int app_bar_main = 2130968604;
+			public const int content_local = 2130968604;
 			
 			// aapt resource value: 0x7f04001d
 			public const int content_main = 2130968605;
@@ -3360,6 +3366,9 @@ namespace Capstone
 			
 			// aapt resource value: 0x7f090026
 			public const int status_bar_notification_info_overflow = 2131296294;
+			
+			// aapt resource value: 0x7f09002b
+			public const int thisIsTheWhereTheWiFiDataWillBeDisplayed = 2131296299;
 			
 			static String()
 			{
