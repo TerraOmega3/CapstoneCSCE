13e55ff1afe647be9c3dded4259a73b845e3f48f
Joshua Langley <joshua.s.langley@gmail.com>
2018-10-11 13:15:49 -0500

    Edited RSSI gathering to utilize AP Scan instead. Added Geolocation function to find current coordinates which are accurate up to a meter. List now displays the first 3 APs within close proximity

--- Capstone/MainActivity.cs
+++ Capstone/MainActivity.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
+using Android.Locations;
 using Android.Net;
 using Android.Net.Wifi;
 using Android.OS;
@@ -11,24 +14,28 @@ using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Plugin.Geolocator;
+
 namespace Capstone
 {
     [Activity(Label = "EyeFi", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         //Change this to your own network ID name or in the schools case "tamulink-wpa"
-        const string networkSSID = "\"" + "Rise.Synergy.Wifi.com" + "\"";
-        
+        const string networkSSID = "\"" + "tamulink-wpa" + "\"";
+       
         TextView wifiText;
         Button wifiButton;
         WifiManager wifiManager;
+        public IList<ScanResult> scanResults;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
             //Creates main page layout by referencing activity_main.axml
             SetContentView(Resource.Layout.activity_main);
 
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 10;
             //Everything below simply connects to wifi given the prewritten networkSSID. To test on your own wifi just change it to your wifiName
             var conf = new WifiConfiguration();
             conf.Ssid = networkSSID;
@@ -45,7 +52,7 @@ namespace Capstone
                 var mawifi = (WifiManager)GetSystemService(WifiService);
                 mawifi.SetWifiEnabled(true);
             }
-            Thread.Sleep(5000);
+            
             wifiManager = (WifiManager)GetSystemService(WifiService);
             wifiManager.AddNetwork(conf);
             var list = wifiManager.ConfiguredNetworks;
@@ -60,22 +67,33 @@ namespace Capstone
                     break;
                 }
             }
-
-            //Number of levels for signal strength
-            int numlevels = 5;
+            
             //Gather WifiInfo data
             WifiInfo wifiInfo = wifiManager.ConnectionInfo;
-            int level = WifiManager.CalculateSignalLevel(wifiInfo.Rssi, numlevels);
+
             //Connect to the main_text on Content_main layer
             wifiText = (TextView)FindViewById(Resource.Id.main_text);
+
+            //Scan all nearby AccessPoints
+            wifiManager.StartScan();
+            scanResults = wifiManager.ScanResults;
+            
+            //Display to main_content the first 3 Accesspoints
+            wifiText.Text = wifiInfo.SSID + "";
+            for (int i = 0; i < 3; i++)
+            {
+                ScanResult AccessPoint = scanResults[i];
+                wifiText.Append("\n AP SSID: " + AccessPoint.Bssid + "\n RSSI: " + AccessPoint.Level);
+            }
             //Chain text to include wifi info items (Note: rssi, BSSID, and linkspeed can all be called after getting wifiInfo)
             // BSSID: AP address, LinkSpeed is the internet speed in Mbps, Rssi is in decibals so from -80 to -10 where -10 is a better connection
-            wifiText.Text = networkSSID + "\n RSSI: " + wifiInfo.Rssi.ToString() + "\n WifiSignal (1-5): "+ level + "\n AP SSID: "+ wifiInfo.BSSID +
-            "\n LinkSpeed: " + wifiInfo.LinkSpeed;
+            
             //Create and connect to button from main_content
             wifiButton = FindViewById<Button>(Resource.Id.button1);
-            //Calls the WFOnClick() function on press
-            wifiButton.Click += WFOnClick;
+
+            //Calls the geolocator function on press (The function also recalls the AP scan)
+            wifiButton.Click += async (object sender, EventArgs args) => { await findPosition(sender, args); };
+
             //Displays a three dot vertical button widget which displays a list of actions (in this case none at the moment)
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
@@ -95,7 +113,27 @@ namespace Capstone
             NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
             navigationView.SetNavigationItemSelectedListener(this);
         }
-
+        //Function to find current GPS coordinates for footprinting use
+        async Task findPosition(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            //How accurate to the meter. IE 500 would result in coordinates being 500 meters within your vicinity
+            locator.DesiredAccuracy = 1;
+            //Get Position
+            var position = await locator.GetPositionAsync(TimeSpan.FromMilliseconds(500));
+            //Edit the Text to include Lat/Long
+            WifiInfo wifiInfo = wifiManager.ConnectionInfo;
+            wifiText.Text = wifiInfo.SSID + "";
+            wifiText.Append("\nLat: " + position.Latitude + "\nLong: " + position.Longitude);
+            //Call the AP scan function
+            wifiManager.StartScan();
+            scanResults = wifiManager.ScanResults;
+            for (int i = 0; i < 3; i++)
+            {
+                ScanResult AccessPoint = scanResults[i];
+                wifiText.Append("\n AP SSID: " + AccessPoint.Bssid + "\n RSSI: " + AccessPoint.Level);
+            }
+        }
         //What happens when someone presses the back button?
         public override void OnBackPressed()
         {
@@ -137,16 +175,6 @@ namespace Capstone
             //Causes a small bar to appear on the bottom that calls either a function or in this case a "replace" statement
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-        private void WFOnClick(object sender, EventArgs eventArgs)
-        {
-            int numlevels = 5;
-            WifiInfo wifiInfo = wifiManager.ConnectionInfo;
-            int level = WifiManager.CalculateSignalLevel(wifiInfo.Rssi, numlevels);
-            wifiText.Text = networkSSID + "\n RSSI: " + wifiInfo.Rssi.ToString() + "\n WifiSignal (1-5): " + level + "\n AP SSID: " + wifiInfo.BSSID + 
-            "\n LinkSpeed: " + wifiInfo.LinkSpeed;
-            //Causes a small bar to appear on the bottom that calls either a function or in this case a "replace" statement
-
         }
         //This is the side bar's function calls
         public bool OnNavigationItemSelected(IMenuItem item)
